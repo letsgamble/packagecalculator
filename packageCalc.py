b4696218529a72013b23b694a4ef5eb0cd20685c
Kamil Wesołowski <letsgamble92@gmail.com>
2022-06-30 23:49:24 +0200

    very early stage

--- packageCalc.py
+++ packageCalc.py
@@ -1,24 +1,25 @@
 # 1 package <= 20 kg
-# 1 element range 1, 10 kg (end if smaller or bigger)
-# if 1 element = 0 kg then end
-#
+# 1 element range 1, 10 kg (end if smaller, bigger or not 0)
+
 # 1. how many packages sent
 # 2. how many kg sent
 # 3. not used space (how many packages * 20 - kg sent)
 # 4. which had most not used space
+
 import sys
+# current_element_weight = int()
 
-current_package_weight = float()
-package_max_kg = float(20)
-element_weight = float({})
-elements_number = int(sys.argv[1])
+# while True:
+#     element_weight = int(input())
+#     current_element_weight += element_weight
+#     print('Element weight:', element_weight)
+#     print('Total elements weight:', current_element_weight)
 
-for package_max_kg in element_weight:
-    if element_weight == range(1, 10):
-        print(element_weight + "KG element added to the package")
-        current_package_weight += element_weight
-else:
-    print("Next package needed")
+file_name = str(input('Please enter import file name: '))
+element_weight = int()
 
-
-
+with open(file_name) as file:
+    for element_weight in file:
+        print(element_weight, end='')
+        if 'str' in element_weight:
+            break
