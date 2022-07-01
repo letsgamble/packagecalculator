f63d1e9f4198a273c1782dddbc9627cf4609bc37
Kamil Wesołowski <letsgamble92@gmail.com>
2022-07-01 11:57:25 +0200

    beta version

--- packageCalc.py
+++ packageCalc.py
@@ -7,21 +7,32 @@
 # 4. which had most not used space
 
 import sys
-# current_element_weight = int()
 
-# while True:
-#     element_weight = int(input())
-#     current_element_weight += element_weight
-#     print('Element weight:', element_weight)
-#     print('Total elements weight:', current_element_weight)
+tpl = 'Packages sent: {}\nPackages total weight: {} [kg]\nUnused space: {} [kg]\nMost unused space: {} [kg]\n'
 
 current_elements_weight = int()
+package_counter = int()
+package_weight_counter = int()
+most_weight_difference = int()
+weight_difference = int()
+total_unused_space = int()
+filename = input()
 
-with open('in.txt') as file:
+with open(filename) as file:
     for element_weight in file:
-        if 1 < int(element_weight) <= 10:
-            current_elements_weight += int(element_weight)
-            print('Current total weight:', current_elements_weight)
+        element_weight = int(element_weight)
+        if 1 < element_weight <= 10:
+            current_elements_weight += element_weight
+            if current_elements_weight > 20:
+                current_elements_weight -= element_weight
+                weight_difference = 20 - current_elements_weight
+                if weight_difference > most_weight_difference:
+                    most_weight_difference = weight_difference
+                package_weight_counter += current_elements_weight
+                package_counter += 1
+                total_unused_space = package_counter * 20 - package_weight_counter
+                current_elements_weight = element_weight
+                print(tpl.format(package_counter, package_weight_counter, total_unused_space, most_weight_difference))
         else:
-            print('Wrong value provided! Program stopped.')
+            print('Wrong value provided! You broke the program!')
             break
