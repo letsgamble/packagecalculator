05561b98bc2a307863f80e22ed29e0de57b152c1
Kamil Wesołowski <letsgamble92@gmail.com>
2022-07-01 15:34:37 +0200

    final version

--- packageCalc.py
+++ packageCalc.py
@@ -1,14 +1,4 @@
-# 1 package <= 20 kg
-# 1 element range 1, 10 kg (end if smaller, bigger or not 0)
-
-# 1. how many packages sent
-# 2. how many kg sent
-# 3. not used space (how many packages * 20 - kg sent)
-# 4. which had most not used space
-
-import sys
-
-tpl = 'Packages sent: {}\nPackages total weight: {} [kg]\nUnused space: {} [kg]\nMost unused space: {} [kg]\n'
+tpl = "\nPackages sent: {}\nPackages total weight: {} [kg]\nTotal unused space: {} [kg]\nMost unused space: {} [kg]\n"
 
 current_elements_weight = int()
 package_counter = int()
@@ -16,23 +6,39 @@ package_weight_counter = int()
 most_weight_difference = int()
 weight_difference = int()
 total_unused_space = int()
-filename = input()
+last_element = int()
+file_name = 'in.txt'
 
-with open(filename) as file:
+with open(file_name) as file:
+    only_one_package = sum(int(line) for line in file)
+    if only_one_package <= 20:
+        print('Shall we send this', only_one_package, 'KG package alone?')
+
+with open(file_name) as file:
     for element_weight in file:
         element_weight = int(element_weight)
-        if 1 < element_weight <= 10:
+        if 1 <= element_weight <= 10:
             current_elements_weight += element_weight
             if current_elements_weight > 20:
+                last_element = element_weight
+                last_element = last_element
                 current_elements_weight -= element_weight
                 weight_difference = 20 - current_elements_weight
                 if weight_difference > most_weight_difference:
                     most_weight_difference = weight_difference
                 package_weight_counter += current_elements_weight
                 package_counter += 1
+                package_counter = package_counter
                 total_unused_space = package_counter * 20 - package_weight_counter
                 current_elements_weight = element_weight
-                print(tpl.format(package_counter, package_weight_counter, total_unused_space, most_weight_difference))
         else:
             print('Wrong value provided! You broke the program!')
-            break
+
+if last_element > 0:
+    package_counter += 1
+    package_weight_counter += last_element
+    weight_difference = 20 - last_element
+    total_unused_space = package_counter * 20 - package_weight_counter
+    if weight_difference > most_weight_difference:
+        most_weight_difference = weight_difference
+    print(tpl.format(package_counter, package_weight_counter, total_unused_space, most_weight_difference))
