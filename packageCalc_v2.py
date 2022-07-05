5a4dfb91a8be6dce77756349de89caab0c1751ce
Kamil Wesołowski <letsgamble92@gmail.com>
2022-07-05 18:05:32 +0200

    beta version

--- packageCalc_v2.py
+++ packageCalc_v2.py
@@ -1,10 +1,17 @@
 import sys
-tpl = "\nPackages sent: {}\nPackages total weight: {} [kg]\nTotal unused space: [kg]\nMost unused space: [kg]\n"
+
+tpl = "\nPackages sent: {}\nPackages total weight: {} [kg]\nTotal unused space: {} [kg]\n" \
+      "Most unused space package no. {}: {} [kg]\n"
+
 how_many_elements = int(sys.argv[1])
 element_weight = int()
 package_weight = int()
 total_weight_sent = int()
 package_counter = int()
+unused_space = int()
+most_unused_space = int(0)
+not_optimal_weight = int()
+most_unused_space_no = int()
 
 for elements_list in range(1, how_many_elements+1):
     elements_list = int(input())
@@ -14,13 +21,26 @@ for elements_list in range(1, how_many_elements+1):
         print('Value 0 detected. Program stopped, calculations until this point.')
         break
     elif 1 <= element_weight <= 10:
+        total_weight_sent += element_weight
         package_weight += element_weight
         if package_weight >= 20:
-            package_counter += 1
-            print('Packages sent: ', package_counter, total_weight_sent)
-            package_weight = 0
+            if package_weight == 20:
+                package_counter += 1
+                package_weight = 0
+            else:
+                package_counter += 1
+                unused_space = 20 - (package_weight - element_weight)
+                package_weight = element_weight
+                if unused_space > most_unused_space:
+                    most_unused_space = unused_space
+                    most_unused_space_no = package_counter
     else:
-        print('Value less than 1 or more than 10 detected. Program stopped.')
+        print('Value lower than 1 or higher than 10 detected. Program stopped.')
         sys.exit()
 
-print(tpl.format(package_counter, total_weight_sent))
+if package_weight > 0:
+    package_counter += 1
+
+not_optimal_weight = package_counter * 20 - total_weight_sent
+
+print(tpl.format(package_counter, total_weight_sent, not_optimal_weight, most_unused_space_no, most_unused_space))
